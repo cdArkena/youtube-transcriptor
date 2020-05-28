d5178445fe58558adf0992e34ee124d2c0a12ca9
Artemis1872 <dennisaw2000@gmail.com>
2020-05-28 23:21:29 +0700

    [UNSTABLE] Implementing new Web Engine using Chromium using JxBrowser

--- src/main/java/TranscriptController.java
+++ src/main/java/TranscriptController.java
@@ -1,32 +1,50 @@
+import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
+
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.view.javafx.BrowserView;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker.State;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.layout.AnchorPane;
 
 public class TranscriptController extends Controller implements Initializable {
 
     @FXML
-    public WebEngine webEngine;
-    @FXML
-    public WebView video;
-    @FXML
     public ListView<LinkedText> transcript;
+    @FXML
     public Label statusURI;
+    //    @FXML
+    //    public WebView player;
+    @FXML
+    public AnchorPane webPane;
+    BrowserView view;
 
     public void loadWebView(String URI) {
-        webEngine.load(URI);
+        view.getBrowser().navigation().loadUrl(URI);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        webEngine = video.getEngine();
+        EngineOptions options = EngineOptions.newBuilder(HARDWARE_ACCELERATED).build();
+        Engine engine = Engine.newInstance(options);
+
+// Create the Browser
+        Browser browser = engine.newBrowser();
+
+// Create the JavaFX BrowserView component
+        view = BrowserView.newInstance(browser);
+        view.setLayoutX(121);
+        view.setLayoutY(139);
+        view.maxHeight(-10);
+        view.minHeight(400);
+        view.minWidth(600);
+        view.prefHeight(400);
+        view.prefWidth(600);
+        webPane.getChildren().add(view);
     }
 }
