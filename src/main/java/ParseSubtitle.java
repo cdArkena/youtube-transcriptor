7c659f0b17b05f00ead60ac1cc6783f464f389fa
Artemis1872 <dennisaw2000@gmail.com>
2020-05-29 22:48:36 +0700

    [BUGFIX] Text Sync issue with Captions

--- src/main/java/ParseSubtitle.java
+++ src/main/java/ParseSubtitle.java
@@ -33,15 +33,16 @@ public class ParseSubtitle {
                         String embedPrefix = "https://www.youtube.com/embed/%s?&start=%s&autoplay=1&showinfo=0&controls=0&disablekb=1&rel=0";
                         String uri = String.format(embedPrefix, videoId, time);
                         LinkedText lt = new LinkedText(br.readLine().strip(), uri);
+                        lt.setStyle("-fx-font: 14 arial;");
+                        lt.setOnMouseClicked(e -> {
+                            view.getBrowser().navigation().loadUrl(lt.getUri());
+                        });
                         ObservableList<LinkedText> list = transcript.getItems();
-                        if (list.size() == 0 || !(list.get(list.size() - 1).getText()
-                            .equals(lt.getText())) && !lt.getText().equals("")) {
-                            lt.setStyle("-fx-font: 14 arial;");
-                            lt.setOnMouseClicked(e -> {
-                                view.getBrowser().navigation().loadUrl(lt.getUri());
-                            });
-                            transcript.getItems().add(lt);
+                        if (list.size() != 0 && (list.get(list.size() - 1).getText()
+                            .equals(lt.getText()))) {
+                            list.remove(list.size() - 1);
                         }
+                        list.add(lt);
                     }
                     line = br.readLine();
                 }
